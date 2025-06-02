d599896d6e8b753cd5597be64b7c032868c971fb
lerianoenzo1988 <lerianoenzo1988@gmail.com>
2025-06-02 05:31:10 +0000

    ultimo ajuste da função checkItem

--- .idx/assets/style.css/main.js
+++ .idx/assets/style.css/main.js
@@ -53,12 +53,7 @@ items.splice(itemIdex, 1)
 
 function checkItem(itemName) {
    const item = items.find((item) => item.name === itemName)
-
-   if (item.checked === true) {
-      item.checked = false
-   } else {
-      item.checked = true
-   }
+   item.checked = !item.checked
 
    showItemsList()
 }
