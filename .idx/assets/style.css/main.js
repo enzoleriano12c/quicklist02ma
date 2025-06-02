cc09d207665bafd4bfeb7f07efa96163e6fcfab0
lerianoenzo1988 <lerianoenzo1988@gmail.com>
2025-06-02 05:48:05 +0000

    adição da verificação da input para não criar itens sem nome

--- .idx/assets/style.css/main.js
+++ .idx/assets/style.css/main.js
@@ -3,6 +3,10 @@ function addItem() {
 
     const itemName = document.querySelector("#item").value
  }
+ if (itemName === "") {Add  
+    alert("Digite um item válido")
+    return
+ }
  const item = {
     name: itemName,
     checked: false
